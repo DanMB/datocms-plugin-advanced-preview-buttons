def37c1d7f473308d59797bd9c653abc4d2ae8fc
Daniel <daniel@setsnail.com>
2024-05-09 21:43:55 +0200

    folder structure

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '',
+	base: './',
+	root: './src',
 	build: {
-		outDir: 'build',
+		outDir: '../build',
+		assetsDir: './',
 		emptyOutDir: true,
 	},
 });
